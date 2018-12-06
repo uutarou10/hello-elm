01d493486cb0956ec2c0de7eba0a41caed5ac26f
Kota Nonaka <kota.kaicho@gmail.com>
2018-12-06 19:56:36 +0900

    format

--- src/ajax.elm
+++ src/ajax.elm
@@ -1,45 +1,54 @@
+module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
+
 import Browser
-import Http
 import Html exposing (Html, pre, text)
+import Http
+
 
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
 
 type Model
-  = Fail
-  | Loading
-  | Success String
+    = Fail
+    | Loading
+    | Success String
 
-init : () -> (Model, Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Loading
-  , Http.get
-      { url = "https://qiita.com/api/v2/users/uutarou10"
-      , expect = Http.expectString Received
-      }
-  )
+    ( Loading
+    , Http.get
+        { url = "https://qiita.com/api/v2/users/uutarou10"
+        , expect = Http.expectString Received
+        }
+    )
+
 
 type Msg
-  = Received (Result Http.Error String)
+    = Received (Result Http.Error String)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Received result ->
-      case result of
-        Ok string ->
-          ( Success string
-          , Cmd.none
-          )
-        Err _ ->
-          ( Fail
-          , Cmd.none
-          )
+    case msg of
+        Received result ->
+            case result of
+                Ok string ->
+                    ( Success string
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( Fail
+                    , Cmd.none
+                    )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -48,10 +57,12 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  case model of
-    Fail ->
-      text "hoge"
-    Loading ->
-      text "Loading..."
-    Success response ->
-      pre [] [text response]
+    case model of
+        Fail ->
+            text "hoge"
+
+        Loading ->
+            text "Loading..."
+
+        Success response ->
+            pre [] [ text response ]
--- src/textField.elm
+++ src/textField.elm
@@ -1,37 +1,48 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
 import Html exposing (Html, div, input, p, text)
 import Html.Attributes exposing (value)
 import Html.Events exposing (onInput)
-import Browser
+
 
 type alias Model =
-  { content: String
-  }
+    { content : String
+    }
+
+
 
 -- initはModel型の変数?
+
+
 init : Model
 init =
-  { content = ""
-  }
+    { content = ""
+    }
 
-type Msg =
-  Change String
+
+type Msg
+    = Change String
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Change str ->
-      { model | content = str }
+    case msg of
+        Change str ->
+            { model | content = str }
+
 
 view : Model -> Html Msg
 view model =
-  div []
-  [ input [value model.content, onInput Change ] []
-  , p [] [text model.content]
-  ]
+    div []
+        [ input [ value model.content, onInput Change ] []
+        , p [] [ text model.content ]
+        ]
+
 
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
